94493bc6ac20506604d024987acd11b03c476882
503Jon503 <je1103637@gmail.com>
2026-07-03 22:32:40 -0600

    Guia 1 completa

---
+++ src/Ejercicio4/SumaEnteros.java
@@ -0,0 +1,36 @@
+package Ejercicio4;
+
+import javax.swing.JOptionPane;
+
+public class SumaEnteros {
+
+    public static void main(String[] args) {
+
+        String primernumero;
+        String segundonumero;
+        int numero1, numero2;
+        int suma;
+
+        primernumero = JOptionPane.showInputDialog("Digite el primer numero");
+        segundonumero = JOptionPane.showInputDialog("Digite el segundo numero");
+
+        numero1 = Integer.parseInt(primernumero);
+        numero2 = Integer.parseInt(segundonumero);
+
+        // Validar números negativos
+        if (numero1 < 0 || numero2 < 0) {
+            JOptionPane.showMessageDialog(null,
+                    "No se pueden ingresar números negativos.");
+            System.exit(0);
+        }
+
+        suma = numero1 + numero2;
+
+        JOptionPane.showMessageDialog(null,
+                "La suma es: " + suma,
+                "Resultado",
+                JOptionPane.PLAIN_MESSAGE);
+
+        System.exit(0);
+    }
+}
